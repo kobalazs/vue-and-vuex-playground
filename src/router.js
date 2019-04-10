d262c44a212e0055001eb4c277dc661f9278a3fa
Balázs Kovács <kobalazs@gmail.com>
2019-04-10 17:23:32 +0200

    Add Registration, Login & NotFound views

--- src/router.js
+++ src/router.js
@@ -14,12 +14,19 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue'),
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: () => import('./views/Registration.vue'),
+    },
+    {
+      path: '*',
+      name: 'not-found',
+      component: () => import('./views/NotFound.vue'),
     },
   ],
 });
