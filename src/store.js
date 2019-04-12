9a66cfbbf09064276aa73a21a596ae62e3588d83
Balázs Kovács <balazs.kovacs@stylersonline.com>
2019-04-12 11:34:11 +0200

    Implement store persistence using vuex-persist

--- src/store.js
+++ src/store.js
@@ -1,14 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 import AuthModule from './modules/AuthModule';
 import TaskModule from './modules/TaskModule';
 
 Vue.use(Vuex);
 
+const persistence = new VuexPersistence({
+  storage: window.localStorage,
+});
+
 export default new Vuex.Store({
   modules: {
     auth: AuthModule,
     task: TaskModule,
   },
+  plugins: [
+    persistence.plugin,
+  ],
 });
